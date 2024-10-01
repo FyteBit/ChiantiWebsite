70eaa8a3dca62df0ab723d45e4556a9c8c8b0e36
FyteBit <Fytebitsoftware@gmail.com>
2024-10-01 16:06:09 +0500

    ...

--- src/app/components/Form/Form.jsx
+++ src/app/components/Form/Form.jsx
@@ -15,14 +15,15 @@ const Form = () => {
 
 
     return (
-        <form className={styles.container}>
+        <section className={styles.container}>
+            <img className={styles.image} src="./form_image.jpg"></img>
             <h2>Оставить заявку</h2>
-            <input type="text" placeholder="Имя" onChange={(event)=>{setData({name:event.target.value})}} className={styles.name}></input>
-            <input type="number" placeholder="phone number" onChange={(event)=>{setData({number:event.target.value})}} className={styles.number}></input>
-            <input type="text" placeholder="Комментарий" onChange={(event)=>{setData({comment:event.target.value})}} className={styles.comment}></input>
+            <input type="text" placeholder="Имя" onChange={(event)=>{setData({name:event.target.value, number: data.number, comment:data.comment})}} className={styles.name}></input>
+            <input type="number" placeholder="Номер телефона" onChange={(event)=>{setData({name:data.name,number:event.target.value,comment:data.comment })}} className={styles.number}></input>
+            <input type="text" placeholder="Комментарий" onChange={(event)=>{setData({name:data.name,number:data.number,comment:event.target.value})}} className={styles.comment}></input>
 
-            <button type="submit" onClick={buttonFunc} className={styles.button}>Отправить</button>
-        </form>
+            <button  onClick={buttonFunc} className={styles.button}>Отправить</button>
+        </section>
     );
 };
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,7 +5,7 @@ import styles from "./Modal.module.css"
 const Modal = ({active, setActive, children}) => {
     return(
         <div className={active ? `${styles.modal} ${styles.active}` : styles.modal} onClick={()=>setActive(false)}>
-        <div className={styles.modalContent} onClick={()=>{}}>{children}</div>
+        <div className={styles.modalContent} onClick={(event)=>{event.stopPropagation()}}>{children}</div>
         </div>
         )
 };
